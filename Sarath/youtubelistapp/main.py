b1d96ef6c054bab35adb11aeec217596a6c9cbf1
Sarath Shekkizhar <shekkizh@usc.edu>
2016-03-06 22:38:00 -0800

    Added google api client lib to local project

--- Sarath/youtubelistapp/main.py
+++ Sarath/youtubelistapp/main.py
@@ -1,17 +1,21 @@
-import os
+import os, sys, inspect
 import urllib
 import webapp2
-# import jinja2
+import jinja2
 
-from apiclient.discovery import build
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0],"lib")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
+
+from googleapiclient.discovery import build
 from optparse import OptionParser
 
-# JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)), extensions=['jinja2.ext.autoescape'])
+JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)), extensions=['jinja2.ext.autoescape'])
 
 # Set DEVELOPER_KEY to the "API key" value from the "Access" tab of the
 # Google APIs Console http://code.google.com/apis/console#access
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "REPLACE_ME"
+DEVELOPER_KEY = "AIzaSyCq_AkZGuFIIgG97ibj7WZNA5F5L4VqEqI"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
@@ -22,7 +26,7 @@ class MainHandler(webapp2.RequestHandler):
             self.response.write("""You must set up a project and get an API key to run this project.  Please visit <landing page> to do so.""")
         else:
             youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
-            search_response = youtube.search().list(q="Hello", part="id,snippet", maxResults=5).execute()
+            search_response = youtube.search().list(q="Danga Maari", part="id,snippet", maxResults=10).execute()
 
             videos = []
             channels = []
@@ -42,9 +46,8 @@ class MainHandler(webapp2.RequestHandler):
                 'playlists': playlists
             }
 
-            # self.response.headers['Content-type'] = 'text/plain'
-            # template = JINJA_ENVIRONMENT.get_template('index.html')
-            # self.response.write(template.render(template_values))
-            self.response.write("Hello Sarath")
+            self.response.headers['Content-type'] = 'text/plain'
+            template = JINJA_ENVIRONMENT.get_template('index.html')
+            self.response.write(template.render(template_values))
 
-app = webapp2.WSGIApplication([('/', MainHandler), ], debug=True)
+app = webapp2.WSGIApplication([('/.*', MainHandler), ], debug=True)
